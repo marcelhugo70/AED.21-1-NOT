25ab07b4a710432b0b74c32f8d42fa2feeda1292
Marcel <marcel@furb.br>
2021-06-14 15:25:08 -0300

    Unidades 5 e 6.

---
+++ src/ordenacao/ClasseDePesquisa.java
@@ -0,0 +1,14 @@
+package ordenacao;
+
+public class ClasseDePesquisa<T extends Comparable<T>> {
+	
+	public int pesquisaLinear(T[] vetor, T elem) {
+		// TODO
+		return -1;
+	}
+	
+	public int buscaBinaria(T[] vetor, T elem) {
+		// TODO
+		return -1;
+    }
+}
